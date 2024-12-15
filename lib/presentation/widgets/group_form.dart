e2fbf5496537684ac6e4b08f7ccebf411dd1fb2e
inpt333 <inpt333@gmail.com>
2024-12-15 07:09:20 +0100

    fix(reply-buttons): fix breaking issue on web with reply buttons

--- lib/presentation/widgets/group_form.dart
+++ lib/presentation/widgets/group_form.dart
@@ -3,7 +3,6 @@ import 'package:flutter/services.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:parousia/models/group.dart';
-import 'package:uuid/v7.dart';
 
 class GroupForm extends StatefulWidget {
   final ValueSetter<Group> onSave;
