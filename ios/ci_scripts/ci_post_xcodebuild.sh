b601925bf2b44493fe1403c7b9000eefdcf33d5e
Giorgio Azzinnaro <giorgio@azzinna.ro>
2025-01-01 12:35:21 +0100

    ci(ios): fix missing dart on post build

--- ios/ci_scripts/ci_post_xcodebuild.sh
+++ ios/ci_scripts/ci_post_xcodebuild.sh
@@ -4,9 +4,12 @@
 set -e
 
 # The default execution directory of this script is the ci_scripts directory.
-cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
+cd "$CI_PRIMARY_REPOSITORY_PATH" # change working directory to the root of your cloned repo.
 
-# Run Sentry dart plugin.
+# Make Flutter and Dart available in the PATH (it was already installed in the ci_post_clone.sh script)
+export PATH="$PATH:$HOME/flutter/bin"
+
+# Run Sentry Dart plugin to upload source maps and debug symbols to Sentry.
 dart run sentry_dart_plugin
 
 exit 0
